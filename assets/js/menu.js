4507fb2261244398874791f0a80c3d29d7445d26
ClaudiaPrez88 <58665197+ClaudiaPrez88@users.noreply.github.com>
2025-10-17 15:11:31 -0300

    arreglo de responsividad del menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,19 +1,6 @@
- // Toggle sidebar
- const toggleButton = document.getElementById('menu-toggle');
-      const sidebar = document.getElementById('sidebar');
-let userToggled = false;
+const toggleButton = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
 
-       function setInitialSidebarState() {
-        if (window.innerWidth < 768) {
-          sidebar.classList.add('toggled');
-        } else {
-          sidebar.classList.remove('toggled');
-        }
-      }
-      setInitialSidebarState();
-      window.addEventListener('resize', setInitialSidebarState);
-
-      toggleButton.addEventListener('click', () => {
-        sidebar.classList.toggle('toggled');
-      });
-      
+toggleButton.addEventListener('click', () => {
+  sidebar.classList.toggle('toggled');
+});
